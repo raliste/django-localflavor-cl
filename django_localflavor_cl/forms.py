62da328a96a0902a855a77b56d1bcd5657c42404
Rodrigo Aliste P. <rodrigo@joldit.com>
2013-05-01 18:14:27 -0300

    Adds CLPhoneNumberField to Chile form helpers.

--- django_localflavor_cl/forms.py
+++ django_localflavor_cl/forms.py
@@ -12,6 +12,10 @@ from django.utils.encoding import smart_text
 
 from .cl_regions import REGION_CHOICES
 
+
+phone_digits_re = re.compile(r'^(\d{2})[-\.]?(\d{4})[-\.]?(\d{4})$')
+
+
 class CLRegionSelect(Select):
     """
     A Select widget that uses a list of Chilean Regions (Regiones)
@@ -95,3 +99,18 @@ class CLRutField(RegexField):
             code = code[:new_dot] + '.' + code[new_dot:]
         return '%s-%s' % (code, verifier)
 
+
+class CLPhoneNumberField(CharField):
+    default_error_messages = {
+        'invalid': _('Phone numbers must be in XX XXXX XXXX format.')
+    }
+  
+    def clean(self, value):
+        super(CLPhoneNumberField, self).clean(value)
+        if value in EMPTY_VALUES:
+            return ''
+        value = re.sub('(\(|\)|\s+)', '', smart_text(value))
+        m = phone_digits_re.search(value)
+        if m:
+            return '%s %s %s' % (m.group(1), m.group(2), m.group(3))
+        raise ValidationError(self.error_messages['invalid'])
